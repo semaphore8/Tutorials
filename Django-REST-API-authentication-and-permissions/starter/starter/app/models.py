42a7aee16d9742bd2b476662fbb9d320dd43b4f9
sgbliznyuk <sgbliznyuk@gmail.com>
2020-03-11 09:55:30 +0300

    edited 1st tutorial

--- Django-REST-API-authentication-and-permissions/starter/starter/app/models.py
+++ Django-REST-API-authentication-and-permissions/starter/starter/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField('title', max_length=50)
+    summary = models.CharField('summary', max_length=50)
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+
+    def __str__(self):
+        return self.title
